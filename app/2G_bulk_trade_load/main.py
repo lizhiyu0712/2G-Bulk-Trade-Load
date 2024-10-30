3416ebf015fe2fb335d87a413b7f813fda76d109
Zhiyu Li <57452212+lizhiyu0712@users.noreply.github.com>
2024-10-30 00:00:03 -0400

    Change to CX_Freezer packing method

---
+++ app/2G_bulk_trade_load/main.py
@@ -0,0 +1,15 @@
+from cx_Freeze import setup, Executable
+
+setup(
+    name="2G Bulk Trade Load",
+    version="0.1.0",
+    description="",
+    options={
+        "build_exe": {
+            "zip_include_packages": ["*"],  # Include all packages in the zip
+            "zip_exclude_packages": [],     # Exclude none
+            "build_exe": "dist",            # Output directory for the build
+        }
+    },
+    executables=[Executable("2G_bulk_trade_load.py", target_name="2G_bulk_trade_load.exe")]
+)
